baf781c8b2feacacdc4d83118768ad193bd268ec
robertavallelunga <1000003065@studium.unict.it>
2025-02-17 13:24:39 +0100

    ho sistemato delle cose che non funzionavano più in crea utente

---
+++ src/main/java/ui/VisualizzaAppelliPerFeedbackUI.java
@@ -0,0 +1,115 @@
+package ui;
+
+import classi.Appello_esame;
+import classi.Docente;
+import classi.EMS;
+import classi.Insegnamento;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class VisualizzaAppelliPerFeedbackUI implements Initializable {
+    private EMS ems;
+    private Docente docente;
+    private Insegnamento insegnamento;
+    private List<String> feedbacks;
+
+    @FXML
+    private ListView<String> appelliInsegnamentoListView;
+    @FXML
+    private TextField idAppelloTextField;
+    @FXML
+    private ListView<String> feedbackListView;
+    @FXML
+    private Button scegliAppelloButton;
+    @FXML
+    private Button BottoneIndietroInsegnamentiDocente;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ems= EMS.getInstance();
+        docente = ems.getDocenteCorrente();
+        insegnamento=ems.getInsegnamentoSelezionato();
+        visualizzaAppelli();
+    }
+
+    private void visualizzaAppelli() {
+        if (insegnamento != null) {
+            HashMap<String, Appello_esame> exam_list = ems.visualizzaAppelliPerInsegnamento(insegnamento.getID_insegnamento());
+
+            if (exam_list == null || exam_list.isEmpty()) {
+                appelliInsegnamentoListView.getItems().add("Non ci sono appelli per questo insegnamento.");
+                return;
+            }
+
+            for (HashMap.Entry<String, Appello_esame> entry : exam_list.entrySet()) {
+                Appello_esame appello = entry.getValue();
+                String appelloString = appello.getID_appello() + " - " + appello.getData();
+                appelliInsegnamentoListView.getItems().add(appelloString);
+            }
+        }
+    }
+
+    @FXML
+    private void visualizzaFeedback(){
+        String idAppello = idAppelloTextField.getText();
+
+        if (idAppello == null || idAppello.isEmpty()) {
+            showAlert("Errore", "Inserisci l'ID dell'appello.");
+            return;
+        }
+
+        Appello_esame appello = ems.getAppelloById(idAppello); // Implementa questo metodo in EMS, accetta una stringa
+
+        if (appello == null) {
+            showAlert("Errore", "Appello non trovato.");
+            return;
+        }
+
+        if(feedbackListView.getItems() != null && !feedbackListView.getItems().isEmpty()){
+            feedbackListView.getItems().clear();
+        }
+
+
+        feedbacks = appello.getFeedbacks();
+        for (String feedback : feedbacks) {
+            feedbackListView.getItems().add(feedback);
+        }
+    }
+
+
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void IndietroInsegnamentiDocente() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("VisualizzaPrenotatiEsameView.fxml")); // Assicurati che il nome del file sia corretto
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+
+        stage.setTitle("Visualizza prenotati esame");
+        stage.setScene(scene);
+        stage.show();
+
+        Stage currentStage = (Stage) BottoneIndietroInsegnamentiDocente.getScene().getWindow();
+        currentStage.close();
+    }
+}
--- src/main/java/ui/VisualizzaFeedbackEsameUI.java
+++ src/main/java/ui/VisualizzaFeedbackEsameUI.java
@@ -1,18 +1,113 @@
 package ui;
 
+import classi.Docente;
 import classi.EMS;
+import classi.Insegnamento;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class VisualizzaFeedbackEsameUI implements Initializable {
     public VisualizzaFeedbackEsameUI() {}
     private EMS ems;
+    private Docente docente;
+
+    @FXML
+    private ListView<String> insegnamentiDocenteListView;
+    @FXML
+    private TextField codiceInsegnamentoTextField;
+    @FXML
+    private Button scegliInsegnamentoButton;
+    @FXML
+    private Button BottoneIndietroPaginaDocente;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         ems=EMS.getInstance();
+        docente = ems.getDocenteCorrente();
+        visualizzaInsegnamentiDocente();
+    }
+
+    private void visualizzaInsegnamentiDocente() {
+        System.out.println("Docente: " + docente);
+        if (docente != null) {
+            String codiceDocente = docente.getCodiceDocente();
+            List<Insegnamento> insegnamenti = ems.mostraInsegnamentiDocente(codiceDocente);
+
+            if (insegnamenti == null || insegnamenti.isEmpty()) {
+                insegnamentiDocenteListView.getItems().add("Non hai insegnamenti assegnati.");
+                return;
+            }
+
+            for (Insegnamento insegnamento : insegnamenti) {
+                insegnamentiDocenteListView.getItems().add(insegnamento.getID_insegnamento() + " - " + insegnamento.getNome());
+            }
+        }
+    }
+
+    @FXML
+    private void visualizzaAppelliInsegnamento(ActionEvent event) throws IOException {
+        String codiceInsegnamento = codiceInsegnamentoTextField.getText();
+
+        if (codiceInsegnamento == null || codiceInsegnamento.isEmpty()) {
+            showAlert("Errore", "Inserisci il codice dell'insegnamento.");
+            return;
+        }
+
+        Insegnamento insegnamento = ems.getInsegnamento(codiceInsegnamento);
+
+        if (insegnamento == null) {
+            showAlert("Errore", "Insegnamento non trovato.");
+            return;
+        }
+        //SETTO L'INSEGNAMENTO
+        ems.setInsegnamentoSelezionato(insegnamento);
+        apriListaAppelliView(insegnamento);
+    }
+
+    private void apriListaAppelliView(Insegnamento insegnamento) throws IOException {
+        Stage primaryStage = (Stage) codiceInsegnamentoTextField.getScene().getWindow(); // Ottieni lo Stage corrente
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("VisualizzaAppelliPerFeedbackView.fxml")); // Crea un nuovo FXMLLoader
+        Scene scene = new Scene(fxmlLoader.load());
+
+        primaryStage.setScene(scene); // Imposta la nuova scena sullo Stage
+        primaryStage.setTitle("Appelli di " + insegnamento.getNome());
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void IndietroPaginaDocente() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DocenteView.fxml")); // Assicurati che il nome del file sia corretto
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+
+        stage.setTitle("Pagina Docente");
+        stage.setScene(scene);
+        stage.show();
+
+        Stage currentStage = (Stage) BottoneIndietroPaginaDocente.getScene().getWindow();
+        currentStage.close();
     }
 
 }
+
+
